84141f0fbe0fc4b787467334a17ef794cb0d5132
thomas <thomaslenguyenasian@gmail.com>
2021-03-03 09:32:00 -0800

    push
    3/3/2021

--- Lab9.asm
+++ Lab9.asm
@@ -1,10 +1,10 @@
-.data
+	.data
 arr1:	.word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 
 strdiv:	.asciiz "\n================================================================================\n"
 strdes: .asciiz "Program Description: 			Finds the max of the array\n"
 straut: .asciiz "Author: 				Thomas Thuan Le Nguyen\n"
-strdat: .asciiz "Creation Date:				02/28/2021"
+strdat: .asciiz "Creation Date:				03/02/2021"
 
 strtms: .asciiz "How many elements: "
 strmax: .asciiz "The largest element is: "
@@ -12,115 +12,116 @@ strerr: .asciiz "Too many elements!!!\n"
 
 strcol: .asciiz ": "
 strskp: .asciiz "\n"
-.text
+	.text
 
 main:
-  # header start
-  li $v0, 4
-  la $a0, strdiv
-  syscall
+	# header start
+  	li $v0, 4
+  	la $a0, strdiv
+	syscall
 
-  la $a0, strdes
-  syscall
+  	la $a0, strdes
+  	syscall
 
-  la $a0, straut
-  syscall
+  	la $a0, straut
+  	syscall
 
-  la $a0, strdat
-  syscall
+  	la $a0, strdat
+  	syscall
 
-  la $a0, strdiv
-  syscall
-  # header end
+  	la $a0, strdiv
+  	syscall
+  	# header end
 
-  # ask
-  la $a0, strtms
-  syscall
+  	# ask
+  	la $a0, strtms
+  	syscall
 
-  # user's input
-  li $v0, 5
-  syscall
-  add $s1, $0, $v0
+  	# user's input
+  	li $v0, 5
+  	syscall
+  	add $s1, $0, $v0
 
 check:
-bgt $s1, 0, secondCondition
+	bgt $s1, 0, secondCondition
 
-j fail
+	j fail
 
 secondCondition:
 
       ble $s1, 10, works
 
 fail:	# invalid number
-    li $v0, 4
-    la $a0, strerr
-    syscall
+	li $v0, 4
+	la $a0, strerr
+    	syscall
 
-    # ask
-  la $a0, strtms
-    syscall
+    	# ask
+  	la $a0, strtms
+	syscall
 
-    # user's input
-    li $v0, 5
-    syscall
-    add $s1, $0, $v0
+    	# user's input
+    	li $v0, 5
+    	syscall
+	add $s1, $0, $v0
 
-  j check
+  	j check
 
 works:
-add $s1, $0, $v0 # $s1 indicates there are N numbers in the array
+	add $s1, $0, $v0 # $s1 indicates there are N numbers in the array
 
-# set first index to 0
-li $s0, 0
+	# set first index to 0
+	li $s0, 0
 
-la $s2, arr1 # $s2 points to first location of array(index zero) with address arr1
+	la $s2, arr1 # $s2 points to first location of array(index zero) with address arr1
 loop: beq $s0, $s1, next # when the loop is done jump to exit
 
-li $v0, 1
-add $a0, $s0, 1
-syscall
+	li $v0, 1
+	add $a0, $s0, 1
+	syscall
 
-li $v0, 4
-la $a0, strcol
-syscall
+	li $v0, 4
+	la $a0, strcol
+	syscall
 
-li $v0, 5 # Get input from user
-syscall
+	li $v0, 5 # Get input from user
+	syscall
 
-sw $v0, 0($s2) # Store the number into the array
-addi $s0, $s0, 1 # update the counter
-addi $s2, $s2, 4 # increment $s2 (the array pointer) by 4 to point to the next position
-j loop
+	sw $v0, 0($s2) # Store the number into the array
+	addi $s0, $s0, 1 # update the counter
+	addi $s2, $s2, 4 # increment $s2 (the array pointer) by 4 to point to the next position
+	j loop
 
 next:
 
-# set first index to 0
-li $s0, 0
+	# set first index to 0
+	li $s0, 0
 
-la $s2, arr1 # $s2 points to first location of array(index zero) with address arr1
-lw $t1, 0($s2)
+	la $s2, arr1 # $s2 points to first location of array(index zero) with address arr1
+	lw $t1, 0($s2)
 
 max: beq $s0, $s1, end # when the loop is done jump to exit
-lw $t0, 0($s2) # Take the element out of array and load it to register $t0
-bge $t1, $t0, false
-add $t1, $0, $t0
+
+	lw $t0, 0($s2) # Take the element out of array and load it to register $t0
+	bge $t1, $t0, false
+	add $t1, $0, $t0
 
 false:
 
-addi $s0, $s0, 1 # update the counter
-addi $s2, $s2, 4 # increment $s2 (the array pointer) by 4 to point to the next position
-j max
+	addi $s0, $s0, 1 # update the counter
+	addi $s2, $s2, 4 # increment $s2 (the array pointer) by 4 to point to the next position
+	j max
 
 end:
 
-li $v0, 4
-la $a0, strmax
-syscall
+	li $v0, 4
+	la $a0, strmax
+	syscall
 
-li $v0, 1
-add $a0, $t1, $0
-syscall
+	li $v0, 1
+	add $a0, $t1, $0
+	syscall
 
-#program end
-  li $v0, 10
-  syscall
+	#program end
+  	li $v0, 10
+  	syscall
